7ec2ce6fed05541ef04231136e38df36e4a3d3ce
Basil Vazhathottathil <117192374+basil-vazhathottathil@users.noreply.github.com>
2023-12-08 11:26:44 +0530

    hash.c

--- hash.c
+++ hash.c
@@ -1,75 +1,69 @@
 #include <stdio.h>
-#include<stdlib.h>
-
+#include <stdlib.h>
 
 #define TABLE_SIZE 5
 
-int h[TABLE_SIZE]={0};
+int h[TABLE_SIZE] = {0};
 
-void insert()
-{
+void insert() {
+    int key, index, i, flag = 0, hkey;
+    printf("\nEnter a value to insert into the hash table: ");
+    scanf("%d", &key);
 
- int key,index,i,flag=0,hkey;
- printf("\nenter a value to insert into hash table\n");
- scanf("%d",&key);
- hkey=key%TABLE_SIZE;
- for(i=0;i<TABLE_SIZE;i++)
-    {
-
-     index=(hkey+i)%TABLE_SIZE;
-
-     if(h[index] == 0)
-     {
-        h[index]=key;
-         break;
-     }
-
+    // Check for duplicate values
+    for (i = 0; i < TABLE_SIZE; i++) {
+        if (h[i] == key) {
+            printf("\nDuplicate element. Cannot insert.\n");
+            return;
+        }
     }
 
-    if(i == TABLE_SIZE)
-
-     printf("\nelement cannot be inserted\n");
-}
-void search()
-{
-
- int key,index,i,flag=0,hkey;
- printf("\nenter search element\n");
- scanf("%d",&key);
- hkey=key%TABLE_SIZE;
- for(i=0;i<TABLE_SIZE; i++)
- {
-    index=(hkey+i)%TABLE_SIZE;
-    if(h[index]==key)
-    {
-      printf("value is found at index %d",index);
-      break;
+    hkey = key % TABLE_SIZE;
+    for (i = 0; i < TABLE_SIZE; i++) {
+        index = (hkey + i) % TABLE_SIZE;
+        if (h[index] == 0) {
+            h[index] = key;
+            break;
+        }
     }
-  }
-  if(i == TABLE_SIZE)
-    printf("\n value is not found\n");
+
+    if (i == TABLE_SIZE)
+        printf("\nElement cannot be inserted.\n");
 }
-void display()
-{
 
-  int i;
+void search() {
+    int key, index, i, flag = 0, hkey;
+    printf("\nEnter the search element: ");
+    scanf("%d", &key);
+    hkey = key % TABLE_SIZE;
 
-  printf("\nelements in the hash table are \n");
+    for (i = 0; i < TABLE_SIZE; i++) {
+        index = (hkey + i) % TABLE_SIZE;
+        if (h[index] == key) {
+            printf("Value is found at index %d\n", index);
+            return;
+        }
+    }
 
-  for(i=0;i< TABLE_SIZE; i++)
-
-  printf("\nat index %d \t value =  %d",i,h[i]);
-
+    printf("\nValue is not found.\n");
 }
-int main()
-{
-    int opt,i;
-    while(1)
-    {
-        printf("\nPress 1. Insert\t 2. Display \t3. Search \t4.Exit \n");
-        scanf("%d",&opt);
-        switch(opt)
-        {
+
+void display() {
+    int i;
+    printf("\nElements in the hash table are:\n");
+
+    for (i = 0; i < TABLE_SIZE; i++)
+        printf("At index %d \t Value = %d\n", i, h[i]);
+}
+
+int main() {
+    int opt, i;
+
+    while (1) {
+        printf("\nPress 1. Insert\t 2. Display \t3. Search \t4. Exit \n");
+        scanf("%d", &opt);
+
+        switch (opt) {
             case 1:
                 insert();
                 break;
@@ -79,7 +73,10 @@ int main()
             case 3:
                 search();
                 break;
-            case 4:exit(0);
+            case 4:
+                exit(0);
         }
     }
+
+    return 0;
 }
